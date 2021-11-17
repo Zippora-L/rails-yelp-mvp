94cc2e3e8eccee967126a7dec4aac5cb5fd8dfd8
Zippora Latzko <zlatzko@googlemail.com>
2021-11-17 13:56:09 -0300

    validations done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning up DB..."
+Restaurant.destroy_all
+puts "Done!"
+
+puts "Seeding restaurants..."
+
+50.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone_with_country_code,
+    category: Review::CATEGORY.sample
+  )
+  restaurant.save!
+  puts "Restaurant #{restaurant.id} saved to database"
+end
+
+puts "Done!"
